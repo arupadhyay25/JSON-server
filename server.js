9fab104ddd4c0b83bb3ed2f135a58383f224b95c
Madhusudan Prajapat <103635175+Madhusudan0906@users.noreply.github.com>
2022-08-25 00:02:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@
 
 // server.listen(port);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const path = require('path');
 const jsonServer = require('json-server');
 const server = jsonServer.create();

7f13c5036b9d9d9219a197558cd5e7b61582fc01
Madhusudan0906 <prajapatmadhusudan06@gmail.com>
2022-08-24 23:28:59 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,11 @@
+const carouselTopBB = require("./data/Carousel");
+const carouselBottomBB = require("./data/carouselBottomBB");
+const offers = require("./data/offers");
+const storeProducts = require("./data/storeProducts.json");
+
+module.exports = {
+  carouselTopBB,
+  carouselBottomBB,
+  offers,
+  storeProducts
+}

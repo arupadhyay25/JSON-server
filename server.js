167b122378d3b663085b9c30806f0a604daf6449
Madhusudan Prajapat <103635175+Madhusudan0906@users.noreply.github.com>
2022-08-25 00:04:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,3 @@
-// const data = require("./db.js");
-
-// const jsonServer = require("json-server");
-// const server = jsonServer.create();
-// const router = jsonServer.router(data);
-// const middlewares = jsonServer.defaults();
-// const port = process.env.PORT || 3000;
-
-// server.use(middlewares);
-// server.use(router);
-
-// server.listen(port);
-
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 const jsonServer = require('json-server');
